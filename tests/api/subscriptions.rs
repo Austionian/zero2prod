b6ed8356553aec4cc85eb66d49ad683a9b37d0f9
Austin <austin@r00ks.io>
2022-05-27 14:26:22 -0500

    migrate db and email response

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,9 +1,18 @@
 use crate::helpers::spawn_app;
+use wiremock::matchers::{method, path};
+use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
-    let body = "name=rooks&email=rooks%40rooks.io";
+    let body = "name=rooks&email=rooks@rooks.io";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
     let response = app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
@@ -19,19 +28,23 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
     let app = spawn_app().await;
     let test_cases = vec![
-        ("name=rooks", "missing the email"),
-        ("email=rooks%40rooks.io", "missing the name"),
-        ("", "missing both the name and emial"),
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
     ];
 
     for (invalid_body, error_message) in test_cases {
+        // Act
         let response = app.post_subscriptions(invalid_body.into()).await;
 
+        // Assert
         assert_eq!(
             400,
             response.status().as_u16(),
+            // Additional customised error message on test failure
             "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
         );
@@ -56,4 +69,19 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             description
         )
     }
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_for_valid_data() {
+    let app = spawn_app().await;
+    let body = "name=rooks&email=austin@r00ks.io";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
 }
