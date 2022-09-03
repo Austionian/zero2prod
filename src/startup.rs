9eb220301e463c107fbdbbd9369a6037fee8a71b
Austin Rooks <Austionian@users.noreply.github.com>
2022-09-03 13:18:27 -0500

    verify query params

--- src/startup.rs
+++ src/startup.rs
@@ -5,6 +5,7 @@ use crate::routes::{
 };
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use secrecy::Secret;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -41,6 +42,7 @@ impl Application {
             connection_pool,
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -68,6 +70,7 @@ fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     // wrap the connection in a smart pointer
     let db_pool = web::Data::new(db_pool);
@@ -86,8 +89,12 @@ fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(web::Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
     Ok(server)
 }
+
+#[derive(Clone, Debug)]
+pub struct HmacSecret(pub Secret<String>);
